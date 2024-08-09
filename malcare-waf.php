f109e18f01c104c2f501bffe6330d9827d59a8a5
ding <ding781204@gmail.com>
2024-08-09 11:31:30 +0000

    first commit for dincystore

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/1306493.cloudwaysapps.com/txnxptrmzw/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php') && !defined("MCDATAPATH")) {
+	define("MCDATAPATH", '/home/1306493.cloudwaysapps.com/txnxptrmzw/public_html/wp-content/mc_data/');
+	define("MCCONFKEY", '71fb99487e2ec92eafc27a179189e788');
+	include_once('/home/1306493.cloudwaysapps.com/txnxptrmzw/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'wG1N3rqya07yA82vwQatNPRdRosUjwtQK0uS7itFfL0VT5xYxLIdCHcVV23XPKrw');
+define('SECURE_AUTH_KEY',  'waTuBMtXdY0KrdA9KUAnuV9I75D1KzDm4FXAAu04sQ1rgF3RzuXX6WTNJMKu6d8e');
+define('LOGGED_IN_KEY',    'qeEmKJomW6NvTpRs431j5CQ2uQN4aFrVrjC6mGGeaMhVjVh7FbwDqG1sJPbn6WV3');
+define('NONCE_KEY',        'UH5adNJb9IrBYomCyIJTRrrivGMIQgbmbfWmS4hu86CBKATxsphQpwixTcy1GvPp');
+define('AUTH_SALT',        'apC4StN7Qo97NWo02a0fnv4vgQtddMuxjChcxR5fTqDJ6Mw6bDpsUo0xYKptodHf');
+define('SECURE_AUTH_SALT', 'VE9H27eAdJCUFmPBvQdjVn9vtfeSSmrxsBXFPYA7wGpep9ySpY9uhr99B8hMdsPT');
+define('LOGGED_IN_SALT',   'dELABfmvEhvqyhAScmzT5UabILMAwBBiP5UJGcD5YmH5fAiAppCT0c0C8hnx8x1v');
+define('NONCE_SALT',       '9nAD4fSBCajnLWCQVN2zSunuiUvQcopzMP1tQExFA0tF9MzK883NTa6aR0EpzS1C');
